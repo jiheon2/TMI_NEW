07b0d751d6af800dfe5d3c864cd313fd48b1e70e
Kim Ji Heon <000224kjh@gmail.com>
2023-10-23 16:49:22 +0900

    수정

--- src/main/java/kopo/poly/test1.java
+++
@@ -1,7 +0,0 @@
-package kopo.poly;
-
-public class test1 {
-    public static void main(String[] args) {
-        System.out.printf("준상이는 천재");
-    }
-}
--- src/main/java/kopo/poly/test2.java
+++
@@ -1,7 +0,0 @@
-package kopo.poly;
-
-public class test2 {
-    public static void main(String[] args) {
-        System.out.printf("준상이 바보?");
-    }
-}
