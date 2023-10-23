1cf09a819b1b7e5cc15c9f6b36bf4bdf008630a8
Kim Ji Heon <000224kjh@gmail.com>
2023-10-23 15:57:17 +0900

    초기 파일

---
+++ src/main/java/kopo/poly/TmiNewApplication.java
@@ -0,0 +1,13 @@
+package kopo.poly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmiNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmiNewApplication.class, args);
+    }
+
+}
