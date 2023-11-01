3f382a8a3c46dcb6da4e41706a63efee6fccc754
chlwnstkd <jun1234755@naver.com>
2023-11-01 22:01:26 +0900

    리뷰 삭제 성공

--- src/main/java/kopo/poly/controller/ReviewController.java
+++ src/main/java/kopo/poly/controller/ReviewController.java
@@ -1,45 +1,51 @@
 package kopo.poly.controller;
 
 import kopo.poly.dto.MsgDTO;
+import kopo.poly.dto.ProductDTO;
 import kopo.poly.dto.ReviewDTO;
 import kopo.poly.service.IReviewService;
+import kopo.poly.service.IShopService;
 import kopo.poly.util.CmmUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Slf4j
 @RequiredArgsConstructor
 @Controller
 public class ReviewController {
     private final IReviewService reviewService;
+    private final IShopService shopService;
 
     @GetMapping(value = "/trader/reviewMng")
     public String getReviewList(HttpSession session, ModelMap model) throws Exception {
         log.info(this.getClass().getName() + ".getReviewList start!");
 
         String id = CmmUtil.nvl((String) session.getAttribute("SS_ID"));
+//        String seq = CmmUtil.nvl(request.getParameter("seq"));
 
         log.info("SS_ID : " + id);
 
         ReviewDTO pDTO = new ReviewDTO();
 
+//        ProductDTO gDTO = new ProductDTO();
+
         pDTO.setTraderId(id);
+//        gDTO.setPid(id);
+//        gDTO.setSeq(seq);
 
         List<ReviewDTO> rList = Optional.ofNullable(reviewService.getReviewList(pDTO)).orElseGet(ArrayList::new);
 
+//        ProductDTO rDTO = Optional.ofNullable(shopService.getGoodsInfo(gDTO)).orElseGet(ProductDTO::new);
         model.addAttribute("rList", rList);
+//        model.addAttribute("rDTO", rDTO);
+
 
         log.info(this.getClass().getName() + ".getReviewList End!");
 
@@ -47,8 +53,8 @@ public class ReviewController {
     }
 
     @ResponseBody
-    @PostMapping(value = "/trader/reviewDelete")
-    public MsgDTO deleteReview(HttpSession session, HttpServletRequest request) {
+    @PostMapping(value = "/trader/deleteReview")
+    public MsgDTO deleteReview(HttpSession session, @RequestBody List<String> checkboxes) {
         log.info(this.getClass().getName() + ".reviewDelete Start!");
 
         String msg = "";
@@ -57,15 +63,17 @@ public class ReviewController {
 
         try {
             String id = CmmUtil.nvl((String) session.getAttribute("SS_ID"));
-            String seq = CmmUtil.nvl(request.getParameter("seq"));
+//            ArrayList<String> checkboxes = CmmUtil.nvl(request.getParameter("checkboxes"));
 
             log.info("id : " + id);
-            log.info("seq : " + seq);
+            log.info("checkboxes : " + checkboxes);
 
             ReviewDTO pDTO = new ReviewDTO();
-            pDTO.setSeq(seq);
-            pDTO.setTraderId(id);
-            reviewService.deleteReview(pDTO);
+            for(String seq : checkboxes) {
+                pDTO.setSeq(seq);
+                pDTO.setTraderId(id);
+                reviewService.deleteReview(pDTO);
+            }
 
             msg = "삭제되었습니다.";
             res = 1;
