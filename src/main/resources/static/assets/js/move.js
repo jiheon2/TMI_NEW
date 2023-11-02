8a4de4448e9d1c4c27e4c0007748404d2a2df86a
jiheon <000224kjh@gmail.com>
2023-11-02 17:35:01 +0900

    231102 HTML 수정 및 이미지 추가

---
+++ src/main/resources/static/assets/js/move.js
@@ -0,0 +1,42 @@
+
+$(document).ready(function() {
+    $("#index").on("click", function() {
+        location.href = "/trader/traderIndex";
+    })
+
+    $("#traderIndex").on("click", function() {
+    location.href = "/trader/traderIndex";
+})
+
+    $("#shopInfo").on("click", function() {
+    location.href = "/trader/shopInfo";
+})
+
+    $("#traderInfo").on("click", function() {
+    location.href = "/trader/traderInfo";
+})
+
+    $("#goodsMng").on("click", function() {
+    location.href = "/trader/goodsMng";
+})
+
+    $("#reviewMng").on("click", function() {
+    location.href = "/trader/reviewMng";
+})
+
+    $("#reservMng").on("click", function() {
+    location.href = "/trader/reservMng";
+})
+
+    $("#chat").on("click", function() {
+    location.href = "/chat/intro";
+})
+
+    $("#customerService").on("click", function() {
+    location.href = "/trader/customerService";
+})
+
+    $("#goodsBuyInfo").on("click", function() {
+    location.href = "/trader/goodsBuyInfo";
+})
+});
