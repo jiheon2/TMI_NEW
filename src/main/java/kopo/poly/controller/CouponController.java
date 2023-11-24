2185285ee4f8383bc56f612c0df920cd1a83f2e9
jiheon <000224kjh@gmail.com>
2023-11-24 16:40:29 +0900

    231124 코드 수정

--- src/main/java/kopo/poly/controller/CouponController.java
+++ src/main/java/kopo/poly/controller/CouponController.java
@@ -1,4 +1,20 @@
 package kopo.poly.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping(value = "/coupon")
 public class CouponController {
+    @GetMapping(value = "/coupon")
+    public String couponPage() {
+        log.info("start!");
+        return "/coupon/coupon";
+    }
+
 }
