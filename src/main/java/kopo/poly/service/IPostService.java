32b66c7134f506ada93703e0018bc1d66e5bbeec
jiheon <000224kjh@gmail.com>
2023-11-24 10:10:08 +0900

    231124 코드 수정

--- src/main/java/kopo/poly/service/IPostService.java
+++ src/main/java/kopo/poly/service/IPostService.java
@@ -5,7 +5,7 @@ import kopo.poly.dto.PostDTO;
 import java.util.List;
 
 public interface IPostService {
-    List<PostDTO> getPostList() throws Exception; // 게시판 목록 조회
+    List<PostDTO> getPostList(String type) throws Exception; // 게시판 목록 조회
     void insertPostInfo(PostDTO pDTO) throws Exception; // 게시글 등록
     PostDTO getPostInfo(PostDTO pDTO) throws Exception; // 게시글 정보 조회
     int getPostNumber() throws Exception; // 게시글 번호 조회
